f238cbbde9bc37be6b1820eecbcd6a220f2db0cd
Michael <michael.j.lukiman@gmail.com>
2019-09-21 22:03:14 -0400

    Transfer to win

---
+++ parse_face.py
@@ -0,0 +1,66 @@
+import argparse
+import ast
+import csv
+import numpy as np
+import operator
+import os
+import pandas as pd
+import re
+
+from collections import defaultdict
+from PIL import Image
+
+vb = True # Verbosity.
+
+p = argparse.ArgumentParser()
+p.add_argument("batch_csv")
+p.add_argument("cols")
+# p.add_argument('names', nargs='+', help='names referring to the questions')
+args = p.parse_args()
+if vb: print('Args:', args)
+
+data = pd.read_csv(args.batch_csv)
+if vb: print(data.columns)
+
+with open(args.cols, "r") as f:
+    cols = ast.literal_eval(f.read())
+
+res = defaultdict(int)
+for ind, row in data.iterrows():
+    for col in cols:
+        if row[col] == True:
+            res[col] += 1 
+
+if vb: print([item for item in res.items()])
+
+most = sorted(res.items(), key=operator.itemgetter(1), reverse=True)
+if vb: print('\n',most)
+
+name_ranking = defaultdict(list)
+
+for tup in most:
+    name, image = tup[0].split('.')[1].split('_')[0:3:2]
+    name_ranking[name].append(image)
+
+if vb: print('\n', name_ranking)
+
+rows = 10
+cols = 1
+
+for key, val in name_ranking.items():
+    x_offset = 0 
+    y_offset = 0
+    image_paths = [os.path.join('..','image_tiler','to_num',str(num)+'.png') for num in val] 
+    dims = Image.open(image_paths[0]).size
+    images = map(Image.open, image_paths)
+    new_im = Image.new('RGB', (int(dims[0]), int(dims[1])*len(val)))
+    for i, im in enumerate(images):
+        print(x_offset, y_offset)
+        new_im.paste(im, (x_offset,y_offset))
+        x_offset = x_offset + dims[0] if (i+1) % rows == 0 else 0
+        if (i+1) % cols == 0: 
+            y_offset += dims[1]
+
+    name = f'{key}.png'
+    print("Generating", name)
+    new_im.resize((dims[0]//4,dims[1]*len(val)//4), Image.ANTIALIAS).save(name, "PNG")
